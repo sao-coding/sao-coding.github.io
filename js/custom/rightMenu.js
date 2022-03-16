420c2143f27ffd9ab91c9476e73117290d3e948e
sao-coding <jeter.nice@gmail.com>
2022-03-16 18:43:14 +0000

    Site updated: 2022-03-16 18:43:14

--- js/custom/rightMenu.js
+++ js/custom/rightMenu.js
@@ -41,25 +41,25 @@ kk.switchReadMode = function(){
 
     newEle.addEventListener('click', clickFn)
 }
-kk.switchTheme=function(load=false){
-    //空字符串表示butterfly原版主题（即不加载css）
-    //FallGuys.css是我自己的魔改主题，需替换
-    let themes = ['FallGuys.css',''];
-    let vTheme = parseInt(localStorage.getItem('visitor-theme'));
-    if(!vTheme){
-        vTheme = load?0:1;
-    }else{
-        vTheme += load?0:1;
-        vTheme%=themes.length;
-    }
-    localStorage.setItem('visitor-theme',vTheme)
-    let themesrc = ''
-    if(themes[vTheme]){
-        themesrc += window.location.origin+'/css/dorakika/'+themes[vTheme];
-    }
-    //css引入时link标签添加属性tag="theme"
-    $(document.head).find('[tag="theme"]')[0].href = themesrc;
-};
+// kk.switchTheme=function(load=false){
+//     //空字符串表示butterfly原版主题（即不加载css）
+//     //FallGuys.css是我自己的魔改主题，需替换
+//     let themes = ['FallGuys.css',''];
+//     let vTheme = parseInt(localStorage.getItem('visitor-theme'));
+//     if(!vTheme){
+//         vTheme = load?0:1;
+//     }else{
+//         vTheme += load?0:1;
+//         vTheme%=themes.length;
+//     }
+//     localStorage.setItem('visitor-theme',vTheme)
+//     let themesrc = ''
+//     if(themes[vTheme]){
+//         themesrc += window.location.origin+'/css/dorakika/'+themes[vTheme];
+//     }
+//     //css引入时link标签添加属性tag="theme"
+//     $(document.head).find('[tag="theme"]')[0].href = themesrc;
+// };
 
 //复制选中文字
 kk.copySelect = function(){
